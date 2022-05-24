43336fdcf358558d1c3737897a8a96791480799f
Priyesh GR <priyesh.gr@alshaya.com>
2022-05-25 00:19:09 +0530

    color changed, font changed, little design updated,poem added

--- js/main.js
+++ js/main.js
@@ -1,30 +1,13 @@
 (function ($) {
     "use strict";
 
-    // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
-            
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
-            }
-        }
-    });
-
-
     // Typed Initiate
     if ($('.typed-text-output').length == 1) {
         var typed_strings = $('.typed-text').text();
         var typed = new Typed('.typed-text-output', {
             strings: typed_strings.split(', '),
-            typeSpeed: 100,
-            backSpeed: 20,
+            typeSpeed: 200,
+            backSpeed: 60,
             smartBackspace: false,
             loop: true
         });
